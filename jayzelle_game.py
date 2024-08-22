4f002ffbe6a2da8c68a5ab13a0cd20072a0ad137
AKDelRey <ak@pangeospatial.com>
2024-08-22 23:47:11 +0800

    Added top scorer

--- jayzelle_game.py
+++ jayzelle_game.py
@@ -23,12 +23,27 @@ if 'btn_lbl' not in st.session_state:
 button_label = st.session_state.btn_lbl
 correct = st.session_state.random_int
 
+@st.dialog("You got a highscore!")
+def highscore_name(lives):
+    name = st.text_input("Name")
+    message = st.text_input("Short Message")
+    if st.button("Submit"):
+        st.session_state.top = {"Name": name, "Lives": lives, "Message": message}
+        st.rerun()
+
 if lives != 0:
     st.session_state.btn_lbl = "Check Answer"
     if st.button(label = f"{button_label}"):
         if love == correct:
             st.balloons()
             st.success("Correct Answer! You won the game!", icon='💁🏻‍♀️')
+
+            if "top" not in st.session_state:
+                highscore_name(lives)
+            else:
+                if lives > st.session_state.top['Lives']:
+                    highscore_name(lives)
+
             st.session_state.btn_lbl = "Start a new game!"
             st.session_state.lives = 10
             st.session_state.random_int = np.random.randint(0,100)
@@ -43,3 +58,9 @@ else:
     if st.button(label = "Restart game?"):
         st.session_state.lives = 10
         st.session_state.random_int = np.random.randint(0,100)
+
+if "top" in st.session_state:
+    st.write("""
+    ### :rainbow[Top Scorer]
+    """)
+    st.write(f"{st.session_state.top['Name']}, lives remaining: {st.session_state.top['Lives']}, message: {st.session_state.top['Message']}")
